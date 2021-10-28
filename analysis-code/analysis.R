d02497ae1dd3b7cc615022079683296599659dd4
kcudding <kcudding@gmail.com>
2021-10-28 15:56:17 -0400

    SOOOO much better with colour

--- analysis-code/analysis.R
+++ analysis-code/analysis.R
@@ -4,9 +4,9 @@ simulated_1 <- rPLB(n = 1000,
                     xmax = 200)
 
 simulated_2 <- rPLB(n = 1000,
-                    xmax = 300)
+                    xmax = 500)
 
 par(mfrow = c(2,1))
 
-hist(simulated_1)
+hist(simulated_1, col="red")
 hist(simulated_2)
