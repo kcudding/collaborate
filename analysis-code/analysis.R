56b750d51f26145f0cf5327e9ab0a92e3ef12f08
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2021-10-28 12:58:04 -0700

    Add some colour and change some values.

--- analysis-code/analysis.R
+++ analysis-code/analysis.R
@@ -1,12 +1,12 @@
 source("../distribution-code/functions.R")
 
-simulated_1 <- rPLB(n = 1000,
+simulated_1 <- rPLB(n = 500,
                     xmax = 200)
 
-simulated_2 <- rPLB(n = 1000,
-                    xmax = 300)
+simulated_2 <- rPLB(n = 300,
+                    xmax = 100)
 
 par(mfrow = c(2,1))
 
-hist(simulated_1)
+hist(simulated_1, col = "blue")
 hist(simulated_2)
